e45b031a2063466c941f2d62387fe490670407e2
yexi_lin <yexi_lin@qq.com>
2025-05-29 15:54:53 +0800

    IDEA commit test

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/**
+* @Author:Yexi_lin
+* @Date: 2025/05/29 15:49
+* @Description:
+*/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
