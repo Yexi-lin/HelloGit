9745cc72a2e744a663c3b210e9c11f50805bb303
yexi_lin <yexi_lin@qq.com>
2025-05-30 11:31:14 +0800

    IDEA commit test

--- Hello.java
+++ Hello.java
@@ -6,5 +6,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("checkout");
+        System.out.println("结账");
     }
 }
